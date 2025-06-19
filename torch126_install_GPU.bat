09409925f8d860d4a2c823b4bd756f2a71d7e55f
RamiSahtout <sahtootrami@yahoo.com>
2025-06-19 12:58:59 +0200

    final

--- torch126_install_GPU.bat
+++
@@ -1,6 +0,0 @@
-:: activate the conda environment
-call conda activate aemntc
-
-:: install PyTorch with CUDA 12.6 support
-call pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
-pause
